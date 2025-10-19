aa89bb614cfa91e711e99f8715fe42105883f0b4
Anish Raut <anishraut.555@gmail.com>
2025-10-20 00:07:34 +0530

    dd

--- app/Utils/URL.js
+++ app/Utils/URL.js
@@ -1,4 +1,1 @@
-export const BASE_URL =
-  typeof window !== "undefined" && window.location.hostname === "localhost"
-    ? "http://localhost:4000"
-    : "/api";
+export const BASE_URL ="https://jobdeskbackend-production.up.railway.app";
